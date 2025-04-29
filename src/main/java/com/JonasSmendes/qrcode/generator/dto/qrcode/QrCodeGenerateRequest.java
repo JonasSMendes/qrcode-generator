a4437b99d06c833909c29cff3fa540bdd3898024
jonas mendes <jonas.jsm2903@gmail.com>
2025-04-29 07:03:45 -0300

    increment: request e response

---
+++ src/main/java/com/JonasSmendes/qrcode/generator/dto/qrcode/QrCodeGenerateRequest.java
@@ -0,0 +1,6 @@
+package com.JonasSmendes.qrcode.generator.dto.qrcode;
+
+public record QrCodeGenerateRequest(
+        String text
+) {
+}
---
+++ src/main/java/com/JonasSmendes/qrcode/generator/dto/qrcode/QrCodeGenerateResponse.java
@@ -0,0 +1,6 @@
+package com.JonasSmendes.qrcode.generator.dto.qrcode;
+
+public record QrCodeGenerateResponse(
+        String url
+) {
+}
